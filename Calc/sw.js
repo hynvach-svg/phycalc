31afb656701bfb87c57e2b829fb3010a07f9afc0
hynvach-svg <hyn.vach@gmail.com>
2026-02-09 17:50:16 +0100

    Add files via upload

---
+++ Calc/sw.js
@@ -0,0 +1,17 @@
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("fyzika-cache").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
